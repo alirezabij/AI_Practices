31c126fc073a6699af54ffd8a9b28915cc2fa616
Alireza Bijandi <alireza_bij777@yahoo.com>
2018-11-18 22:27:54 +0330

    astar done

--- HW1/search.py
+++ HW1/search.py
@@ -136,7 +136,7 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+	return generalGraphSearch(problem, util.PriorityQueue, heuristic)
 
 
 # Abbreviations
